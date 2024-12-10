0335e620701a32c1206df098494c58c74f78799e
Eddy-Nhabinde <edmilsonnhabinde5@gmail.com>
2024-12-10 16:14:06 +0200

    feat: added components on top of the table

--- src/pages/enrollment/enrollment.tsx
+++ src/pages/enrollment/enrollment.tsx
@@ -3,9 +3,10 @@ import React from "react";
 import { headerColumns, rowsData } from "../../utils/constants/table/tableConstants";
 import { RowActionsType } from "dhis2-semis-components/dist/declarations/types/table/TableRowActionsProps";
 import { IconCheckmarkCircle24, IconDelete24, IconEdit24 } from "@dhis2/ui";
+import EnrollmentActionsButtons from "../../components/enrollmentButtons/EnrollmentActionsButtons";
 
 export default function EnrollmentsPage() {
-    
+
     const rowsActions: RowActionsType[] = [
         { icon: <IconEdit24 />, color: '#277314', label: `Edition`, disabled: true, loading: false, onClick: () => { alert("Edition") } },
         { icon: <IconDelete24 />, color: '#d64d4d', label: `Delete`, disabled: false, loading: false, onClick: () => { alert("Delete") } },
@@ -18,20 +19,12 @@ export default function EnrollmentsPage() {
                 viewPortWidth={1040}
                 columns={headerColumns}
                 totalElements={4}
-                loading={false}
-                createSortHandler={() => { }}
-                order={"asc"}
-                orderBy={"fistid1"}
                 tableData={rowsData}
-                sortable={true}
-                isInactive={false}
-                isOwnershipOu={false}
-                showEnrollments={false}
-                showRowActions={true}
                 rowAction={rowsActions}
                 defaultFilterNumber={3}
                 filterState={{ attributes: [], dataElements: [] }}
-                setFilterState={() => { }}
+                title="Enrollments"
+                rightElements={<EnrollmentActionsButtons />}
             />
         </div>
     )
