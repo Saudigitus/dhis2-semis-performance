0ccb9689434df0ce850711ca496bcea710dae1c0
Eddy-Nhabinde <edmilsonnhabinde5@gmail.com>
2025-01-14 10:42:43 +0200

    feat: added filter state in the table

--- src/pages/enrollment/enrollment.tsx
+++ src/pages/enrollment/enrollment.tsx
@@ -1,33 +1,33 @@
 import { ProgramConfig, Table, useDataStoreKey, useProgramsKeys } from "dhis2-semis-components";
-import React, { useEffect } from "react";
-import { headerColumns, rowsData } from "../../utils/constants/table/tableConstants";
+import React, { useEffect, useState } from "react";
 import { RowActionsType } from "dhis2-semis-components/dist/declarations/types/table/TableRowActionsProps";
 import { IconCheckmarkCircle24, IconDelete24, IconEdit24 } from "@dhis2/ui";
 import EnrollmentActionsButtons from "../../components/enrollmentButtons/EnrollmentActionsButtons";
-import { modules, useHeader, useTableData, useUrlParams  } from "dhis2-semis-functions";
+import { modules, useHeader, useTableData, useUrlParams } from "dhis2-semis-functions";
 
 export default function EnrollmentsPage() {
     const { dataStoreValues } = useDataStoreKey();
     const { programsValues } = useProgramsKeys();
     const dataStoreData = dataStoreValues[0]
     const programData = programsValues[0]
-
+    const [filetrState, setFilterState] = useState<{ dataElements: any[], attributes: any[] }>({ attributes: [], dataElements: [] })
     const { getData, tableData, loading } = useTableData({ module: modules.enrollment })
-    const { columns } = useHeader({ dataStoreData, programConfigData: programData as ProgramConfig[0], tableColumns: [], module: modules.enrollment })
-  
+    const { columns } = useHeader({ dataStoreData, programConfigData: programData as ProgramConfig, tableColumns: [], module: modules.enrollment })
+
     const rowsActions: RowActionsType[] = [
         { icon: <IconEdit24 />, color: '#277314', label: `Edition`, disabled: true, loading: false, onClick: () => { alert("Edition") } },
         { icon: <IconDelete24 />, color: '#d64d4d', label: `Delete`, disabled: false, loading: false, onClick: () => { alert("Delete") } },
         { icon: <IconCheckmarkCircle24 />, color: '#147cd7', disabled: false, loading: false, label: 'Complete', onClick: () => { alert("Complete") } }
     ];
 
-    useEffect(() => { 
-        void getData({ page: 1, pageSize: 10, program: programData.id as string, orgUnit: "Shc3qNhrPAz", baseProgramStage: dataStoreData?.registration?.programStage as string, attributeFilters: [], dataElementFilters: [`${dataStoreData?.registration?.academicYear}:in:2024`] })
-    }, [])
+    useEffect(() => {
+        void getData({ page: 1, pageSize: 10, program: programData.id as string, orgUnit: "Shc3qNhrPAz", baseProgramStage: dataStoreData?.registration?.programStage as string, attributeFilters: filetrState.attributes, dataElementFilters: [`${dataStoreData?.registration?.academicYear}:in:2024`] })
+    }, [filetrState])
 
     return (
         <div style={{ height: "80vh" }} >
             <Table
+                programConfig={programData as unknown as any}
                 title="Enrollments"
                 viewPortWidth={1040}
                 columns={columns}
@@ -39,6 +39,7 @@ export default function EnrollmentsPage() {
                 filterState={{ attributes: [], dataElements: [] }}
                 loading={loading}
                 rightElements={<EnrollmentActionsButtons />}
+                setFilterState={setFilterState}
             />
         </div>
     )
