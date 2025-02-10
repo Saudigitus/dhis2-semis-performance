b701313ba9f4acf814bfde6b510312288f8d568b
EdsonNhancale <edsonnhancale97@gmail.com>
2025-02-10 11:45:20 +0200

    feat: solved bug

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,7 +4,7 @@ import { Router } from '../components/routes'
 import { AppWrapper } from 'dhis2-semis-components'
 
 const MyApp = () => {
-
+    console.log("My App")
     return (
         // <AppWrapper dataStoreKey='semis/values'>
         <Router />
--- src/components/routes/Router.tsx
+++ src/components/routes/Router.tsx
@@ -4,10 +4,11 @@ import { EnrollmentsPage } from '../../pages';
 import { FullLayout } from '../../layout';
 
 export default function Router() {
+    console.log("Router")
     return (
         <Routes>
-            <Route path='/semis' element={<FullLayout />}>
-                <Route key={'enrollments'} path={'enrollment'} element={<EnrollmentsPage />} />
+            <Route path='/' element={<FullLayout />}>
+                <Route key={'enrollments'} path={'/'} element={<EnrollmentsPage />} />
             </Route>
         </Routes>
     );
