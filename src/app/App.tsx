712bc0d3383d9938d246a058d7eed98942ac6a37
cnandza <cnandza@saudigitus.org>
2025-05-16 10:25:16 +0200

    remove wrapper

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,13 @@
 import React from 'react'
 import './App.module.css'
 import { Router } from '../components/routes'
-import { AppWrapper } from 'dhis2-semis-components'
 import InitializeWrapper from '../wrapper/InitializeWrapper'
 
 const Performance = () => {
     return (
-        <AppWrapper dataStoreKey='dataStore/semis/values'>
-            <InitializeWrapper>
-                <Router />
-            </InitializeWrapper>
-        </AppWrapper>
+        <InitializeWrapper>
+            <Router />
+        </InitializeWrapper>
     )
 }
 
--- src/components/routes/Router.tsx
+++ src/components/routes/Router.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import { FinalResult } from '../../pages';
-import { Routes, Route, HashRouter } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import WithHeaderBarLayout from '../../layout/WithHeaderBarLayout';
 
 export default function Router() {
     return (
-        <HashRouter>
-            <Routes>
-                <Route path='/' element={<WithHeaderBarLayout />}>
-                    <Route key={'performance'} path={'/'} element={<FinalResult />} />
-                </Route>
-            </Routes>
-        </HashRouter>
+        <Routes>
+            <Route path='/' element={<WithHeaderBarLayout />}>
+                <Route key={'performance'} path={'/'} element={<FinalResult />} />
+            </Route>
+        </Routes>
     );
 }
