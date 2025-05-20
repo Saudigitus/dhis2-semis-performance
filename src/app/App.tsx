5987979b5362d311d711afe68b1ed67899a598b2
Eddy-Nhabinde <edmilsonnhabinde5@gmail.com>
2025-05-20 14:09:55 +0200

    refactor: simplify component structure by removing unnecessary wrappers

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,18 +2,12 @@ import React from 'react'
 import './App.module.css'
 import { Router } from '../components/routes'
 import InitializeWrapper from '../wrapper/InitializeWrapper'
-import { AppWrapper } from 'dhis2-semis-components'
-import { HashRouter } from 'react-router-dom'
 
 const Performance = () => {
     return (
-        <AppWrapper dataStoreKey='dataStore/semis/values' >
-            <InitializeWrapper>
-               <HashRouter>
-                 <Router />
-               </HashRouter>
-            </InitializeWrapper>
-        </AppWrapper>
+        <InitializeWrapper>
+            <Router />
+        </InitializeWrapper>
     )
 }
 
