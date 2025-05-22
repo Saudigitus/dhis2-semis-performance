de16c0e58e8ddab5b53878cfdf4ab97d9292b15d
cnandza <cnandza@saudigitus.org>
2025-05-22 12:41:30 +0200

    update libs version

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,23 +2,12 @@ import React from 'react'
 import './App.module.css'
 import { Router } from '../components/routes'
 import InitializeWrapper from '../wrapper/InitializeWrapper'
-import { AppWrapper } from 'dhis2-semis-components'
-import { HashRouter } from 'react-router-dom'
-import { useConfig } from '@dhis2/app-runtime'
 
 const Performance = () => {
-    const { baseUrl } = useConfig()
     return (
-        <AppWrapper
-            baseUrl={baseUrl}
-            dataStoreKey="dataStore/semis/values"
-        >
-            <InitializeWrapper>
-                <HashRouter>
-                    <Router />
-                </HashRouter>
-            </InitializeWrapper>
-        </AppWrapper>
+        <InitializeWrapper>
+            <Router />
+        </InitializeWrapper>
     )
 }
 
