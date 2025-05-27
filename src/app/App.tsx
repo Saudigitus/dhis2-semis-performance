3a8651302092cffb455a9802b27cc8492fce93b9
cnandza <cnandza@saudigitus.org>
2025-05-27 09:50:02 +0200

    bump version to 1.0.0-alpha.2 and simplify App component structure

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,24 +2,13 @@ import React from 'react'
 import './App.module.css'
 import { Router } from '../components/routes'
 import InitializeWrapper from '../wrapper/InitializeWrapper'
-import { AppWrapper } from 'dhis2-semis-components'
-import { useConfig } from '@dhis2/app-runtime'
-import { HashRouter } from 'react-router-dom'
 
 const Performance = () => {
-    const { baseUrl } = useConfig()
 
     return (
-        <AppWrapper
-            baseUrl={baseUrl}
-            dataStoreKey='dataStore/semis/values'
-        >
-            <InitializeWrapper>
-                <HashRouter>
-                    <Router />
-                </HashRouter>
-            </InitializeWrapper>
-        </AppWrapper>
+        <InitializeWrapper>
+            <Router />
+        </InitializeWrapper>
     )
 }
 
