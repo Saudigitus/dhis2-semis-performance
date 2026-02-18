a5b7580516d823abe632452b6c203fa7497632e4
Edson Nhancale <edsonnhancale97@gmail.com>
2026-02-18 12:22:46 +0200

    refactor(router): pass baseUrl as prop instead of using useConfig hook
    - Remove useConfig hook from WithHeaderBarLayout component
    - Accept baseUrl as prop in Router and WithHeaderBarLayout components
    - Fall back to useConfig baseUrl in Performance component if prop not provided

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,15 +8,16 @@ import { HashRouter } from 'react-router-dom'
 import { D2I18n } from 'dhis2-semis-types'
 import translation from '../locales'
 
-const Performance = ({ i18n }: { i18n: D2I18n }) => {
-    const { baseUrl } = useConfig()
+const Performance = ({ i18n, baseUrl }: { i18n: D2I18n; baseUrl?: string }) => {
+    const { baseUrl: localBaseUrl } = useConfig()
     const translate: any = i18n ? i18n : translation
+    const useBaseUrl = baseUrl || localBaseUrl
 
     return (
-        // <AppWrapper i18n={translate} baseUrl={baseUrl} dataStoreKey='dataStore/semis/values' schoolCalendarKey='dataStore/semis/schoolCalendar'>
+        // <AppWrapper i18n={translate} baseUrl={useBaseUrl} dataStoreKey='dataStore/semis/values' schoolCalendarKey='dataStore/semis/schoolCalendar'>
         //     <InitializeWrapper>
         //         <HashRouter>
-                    <Router i18n={translate} />
+                    <Router i18n={translate} baseUrl={useBaseUrl} />
         //         </HashRouter>
         //     </InitializeWrapper>
         // </AppWrapper>
