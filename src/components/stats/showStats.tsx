f65138f1284e29e16dc0227bf1694c2a2d82c5cf
cnandza <cnandza@saudigitus.org>
2026-04-24 09:41:44 +0200

    fix: update data-test attributes for performance modal and cards

--- src/components/stats/showStats.tsx
+++ src/components/stats/showStats.tsx
@@ -21,7 +21,7 @@ export default function ShowStats({ stats, open, setOpen }: { setOpen: (args: bo
     return (
         <ModalComponent
             open={open}
-            dataTest="show-stats-modal"
+            dataTest="performance-show-stats-modal"
             handleClose={() => setOpen(!open)}
             children={
                 <div>
@@ -32,8 +32,8 @@ export default function ShowStats({ stats, open, setOpen }: { setOpen: (args: bo
                     <WithPadding />
 
                     <ButtonStrip>
-                        <SummaryCard dataTest="show-stats-promoted-students" color="success" label="Promoted students" value={stats?.posted ?? 0} />
-                        <SummaryCard dataTest="show-stats-no-promoted-students" color="error" label="No promoted students" value={stats?.conflicts?.length ?? 0} />
+                        <SummaryCard dataTest="performance-promoted-students-card" color="success" label="Promoted students" value={stats?.posted ?? 0} />
+                        <SummaryCard dataTest="performance-no-promoted-students-card" color="error" label="No promoted students" value={stats?.conflicts?.length ?? 0} />
                     </ButtonStrip>
 
                     <WithPadding />
